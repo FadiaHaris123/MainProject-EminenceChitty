32cd302b4669d3d902d38f372de2ddc74ab6d6cc
FadiaHaris123 <fadia.haris@experionglobal.com>
2022-11-22 18:34:50 +0530

    available chits added

--- src/components/foreman/AvailableChits/AvailableChit.js
+++ src/components/foreman/AvailableChits/AvailableChit.js
@@ -1,41 +1,85 @@
 import { useEffect, useState } from 'react';
 import './AvailableChit.css'
+import ChitList from './ChitList';
+
 const AvailableChit =()=>{
-    const [chits,setchits] = useState([]);
 
-    useEffect(()=>{
-        const fetchChits =async()=>{
-            const response = await fetch(
-                'link'
-            );
-            if (!response.ok) {
-                throw new Error('Something went wrong!');
-              }
-        
-              const responseData = await response.json();
-        
-              const loadedChits = [];
-        
-              const newList = [...responseData._embedded.chittyList]
-              for (const key in newList) {
-                loadedChits.push({
-                  id: key,
-                  name: newList[key].name,
-                  description: newList[key].description,
-                  price: newList[key].price,
-                });
-              }
-        
-            
-            };
-        
-      
-        }
+  const [chits, setChits] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchChits = async () => {
+      const response = await fetch(
+        'http://localhost:8080/api/chittylist'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const responseData = await response.json();
+
+      const loadedChits = [];
+      const newItemList = [...responseData._embedded.chittyList]
+
+      for (const key in newItemList) {
+        loadedChits.push({
+          id: key,
+          name: newItemList[key].chittyName,
+          duration: newItemList[key].duration,
+          totalAmount: newItemList[key].totalAmount,
+        });
+      }
+
+      setChits(loadedChits);
+      setIsLoading(false);
+    };
+
+    fetchChits().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  
+
+
+  if (isLoading) {
+    return (
+      <section className="chitsLoading">
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className="chitsError">
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const chitList = chits.map((chit) => (
+    <ChitList
+      key={chit.id}
+      id={chit.id}
+      name={chit.name}
+      duration={chit.duration}
+      totalAmount={chit.totalAmount}
+    />
+  ));
+
+   return(
+   <div className="container">
+
+       <h4>Available Chits</h4> 
+       <ul>
+        {chitList}
+       </ul>
+
+    </div>
     )
-    return(<div className="container">
-
-        Available Chits
-
-    </div>)
 }
 export default AvailableChit;
---
+++ src/components/foreman/AvailableChits/ChitList.js
@@ -0,0 +1,11 @@
+
+
+const ChitList = (props) => {
+return(
+    <div>
+        <li>{props.name}</li>
+    </div>
+)
+}
+
+export default ChitList;
