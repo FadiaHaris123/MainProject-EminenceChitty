84b5352127dd7c02ffd19f3d5eeee39ee693d1d2
Sarath Kumar S <sarath.s@experionglobal.com>
2022-11-24 08:57:09 +0530

    no updates

--- src/UI/Auth.js
+++ src/UI/Auth.js
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import Image from '../assets/images/login.jpg'
 import '../App.css'
-import foreman from "../components/foreman/foreman"
 
 const Auth = (props) => {
 
