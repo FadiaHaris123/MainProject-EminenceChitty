998cbcd2244860daa11d0db7c175759c204df6cc
Sarath Kumar S <sarath.s@experionglobal.com>
2022-11-25 18:33:33 +0530

    manager css

--- src/components/Manager/ChitDetail.js
+++ src/components/Manager/ChitDetail.js
@@ -5,10 +5,10 @@ import classes from './Chit.module.css';
 const ChitDetail = (props) => {
   return (
     <li className={classes.chit}>
-    <h2>{props.chitType}</h2>
-    <h3>Chit No. :{props.chitNumber}</h3>
-    <p>₹{props.amount}</p>
-  </li>
+    <h2>Members : {props.members}</h2>
+    <h3>Duration :{props.days}</h3>
+    <p>From {props.startDate}</p>
+</li>
   );
 };
 
