2ba491c15dbd99acca2b04c3992e04ffe276feb6
Sarath Kumar S <sarath.s@experionglobal.com>
2022-11-29 10:57:29 +0530

    Customer page

--- src/components/Customer/Customer.js
+++ src/components/Customer/Customer.js
@@ -2,19 +2,11 @@ import React from 'react';
 import Navbar from './Navbar';
 import classes from './Customer.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Auction from './pages/CustomerAuction/Auction';
-import Home from './pages/Home';
 
 function Customer() {
   return (
     <div className= {classes.customer}>
-      <Router>
         <Navbar />
-        <Switch>
-        <Route path='/home' exact component={Home} />
-          <Route path='/auction' component={Auction} />
-        </Switch>
-      </Router>
     </div>
   );
 }
---
+++ src/components/Customer/pages/CustomerProfile/src/pages/Reports.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Reports() {
+  return (
+    <div className='reports'>
+      <h1>Available Chits</h1>
+    </div>
+  );
+}
+
+export default Reports;
